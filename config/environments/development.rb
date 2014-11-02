546113999eebd904bf5f5630cd38f1d672d2ea65
Aleksandar Popovic <web.development.serbia@gmail.com>
2014-11-02 22:46:48 +0100

    add and setup devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,11 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  #Mailcacher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
