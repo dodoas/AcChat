fae60ae2a811e15c857df8715c406092d6264f3f
Aleksandar Popovic <web.development.serbia@gmail.com>
2014-11-04 12:49:32 +0100

    email setings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "ac-chat.herokuapp.com",
+      authentication: "plain",
+      enable_starttls_auto: true,
+      user_name: ENV["iq.ingenium.dev@gmail.com"],
+      password: ENV["dev123456789"]
+  }
+
 end
