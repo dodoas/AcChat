4a356ff55845f47b59563e471288b4bc221609b8
Aleksandar Popovic <web.development.serbia@gmail.com>
2014-11-06 10:03:58 +0100

    no need for registration confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable, :timeoutable
+         :recoverable, :rememberable, :trackable, :validatable#,
+         #:confirmable, :lockable, :timeoutable
   has_many :messages
 end
