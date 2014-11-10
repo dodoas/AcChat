518d43266d7903203138981175b2c896ca9a6c4a
Aleksandar Popovic <web.development.serbia@gmail.com>
2014-11-10 13:46:18 +0100

    del api model

--- app/models/group_email_recipient.rb
+++ app/models/group_email_recipient.rb
@@ -2,4 +2,5 @@ class GroupEmailRecipient < ActiveRecord::Base
   resourcify
   belongs_to :recipient, :class_name => User
   belongs_to :group_email
+  scope :to_group_email, -> (group_email_id) { where( :group_email_id => group_email_id ) }
 end
